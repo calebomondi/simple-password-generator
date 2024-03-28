6286c245a6e431836e7e28b6596f4198000c8811
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-03-28 22:01:27 +0300

    made the source code more precise according to user entry

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -2,31 +2,84 @@ import java.util.Scanner;
 
 public class PasswordGenerator {
     //generate password
-    public static String passwordGen(int size) {
+    public static String passwordGen(int type, int size) {
         String password = "";
-        int num ,max = 126, min = 33;
+        int num, count = 0;;
         char letter;
 
-        int count = 0;
-        while(count < size) {
-            num = (int)Math.floor(Math.random() * (max-min+1) + min);
-            letter = (char)num;
-            password += String.valueOf(letter);
-            count += 1;
+
+        if(type == 1) {
+            while(count < size) {
+                //num = (int)Math.floor(Math.random() * (max-min+1) + min);
+                num = (int)Math.floor(Math.random() * (57-48+1) + 48);
+                letter = (char)num;
+                password += String.valueOf(letter);
+                count += 1;
+            }
+        } else if (type == 2) {
+            while(count < size) {
+                int k = (int)Math.floor(Math.random() * (2-1+1) + 1);
+                if(k == 1) {
+                    num = (int)Math.floor(Math.random() * (90-65+1) + 65);
+                } else {
+                    num = (int)Math.floor(Math.random() * (122-97+1) + 97);
+                }
+                letter = (char)num;
+                password += String.valueOf(letter);
+                count += 1;
+            }
+
+        } else if (type == 3) {
+            while(count < size) {
+                int k = (int)Math.floor(Math.random() * (3-1+1) + 1);
+                if(k == 1) {
+                    num = (int)Math.floor(Math.random() * (90-65+1) + 65);
+                } else if(k == 2){
+                    num = (int)Math.floor(Math.random() * (122-97+1) + 97);
+                } else {
+                    num = (int)Math.floor(Math.random() * (57-48+1) + 48);
+                }
+                letter = (char)num;
+                password += String.valueOf(letter);
+                count += 1;
+            }
+            
+        } else if (type == 4) {
+            while(count < size) {
+                int k = (int)Math.floor(Math.random() * (4-1+1) + 1);
+                if(k == 1) {
+                    num = (int)Math.floor(Math.random() * (90-65+1) + 65);
+                } else if(k == 2){
+                    num = (int)Math.floor(Math.random() * (122-97+1) + 97);
+                }  else if(k == 3){
+                    num = (int)Math.floor(Math.random() * (47-33+1) + 33);
+                }  else {
+                    num = (int)Math.floor(Math.random() * (57-48+1) + 48);
+                }
+                letter = (char)num;
+                password += String.valueOf(letter);
+                count += 1;
+            }
+            
+        } else {
+            System.out.println("Password Type Either 1, 2, 3 or 4!");
         }
 
         return password;
     }
     public static void main(String[] args) {
-        int size = 0;
+        int size, type;
         String pwd = "NULL";
         try (Scanner get = new Scanner(System.in)) {
             //catch InputMismatchError when input is not a digit
+            System.out.println("\nType of password: \n[1] Digits only \n[2] Letters only \n[3] Digits & Letters mixture \n[4] Digits & Letters & Special characters \nMinimum Password Length is 4\n");
             try {
-                System.out.println("Enter the length of the password[min length = 4]: ");
+                System.out.print("Type of password: ");
+                type = get.nextInt();
+                System.out.print("Length of the password: ");
                 size = get.nextInt();
                 if(size >= 4) {
-                    pwd = passwordGen(size);
+                    pwd = passwordGen(type,size);
                 } else {
                     System.out.println("Password Length Should Be greater Than 4!");
                 }
